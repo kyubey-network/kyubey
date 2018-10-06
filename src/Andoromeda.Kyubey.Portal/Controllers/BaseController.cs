e89af079ba0e144f01cdfde496735b4eefc8f60e
Yuko Zheng <1@1234.sh>
2018-10-06 20:54:23 +0800

    Implement publish dex order

--- src/Andoromeda.Kyubey.Portal/Controllers/BaseController.cs
+++ src/Andoromeda.Kyubey.Portal/Controllers/BaseController.cs
@@ -5,5 +5,11 @@ namespace Andoromeda.Kyubey.Portal.Controllers
 {
     public class BaseController : BaseController<KyubeyContext>
     {
+        public override void Prepare()
+        {
+            base.Prepare();
+            ViewBag.OtcContract = Configuration["Contracts:Otc"];
+            ViewBag.DexContract = Configuration["Contracts:Dex"];
+        }
     }
 }
