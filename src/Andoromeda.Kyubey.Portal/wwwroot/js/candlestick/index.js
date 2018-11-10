bd8694bf8158b4189fb88d08ab7c825e37f5de71
qinxiaowen <453000697@qq.com>
2018-11-10 19:45:20 +0800

    set candlestick default cycle to 240(4h)

--- src/Andoromeda.Kyubey.Portal/wwwroot/js/candlestick/index.js
+++ src/Andoromeda.Kyubey.Portal/wwwroot/js/candlestick/index.js
@@ -1,9 +1,2 @@
 
 
-// 单纯的写一个添加class的函数，这个不用看 没用
-function addClass (fatherDom, dom) {
-  [...fatherDom.getElementsByTagName('span')].forEach(function(item){
-    item.className = ''
-  })
-  dom.className = 'active'
-}
