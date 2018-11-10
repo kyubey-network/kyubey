fa8c1baf16f5f822749b6b3d9a542fe4edd90123
Yuko Zheng <1@1234.sh>
2018-11-10 17:17:18 +0800

    Impl candlestick api

---
+++ src/Andoromeda.Kyubey.Portal/Models/Candlestick.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Andoromeda.Kyubey.Portal.Models
+{
+    public class Candlestick
+    {
+        public double Opening { get; set; }
+
+        public double Closing { get; set; }
+
+        public double Max { get; set; }
+
+        public double Min { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
