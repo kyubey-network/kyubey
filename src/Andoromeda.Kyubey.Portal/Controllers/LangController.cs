df5d266b29eace5720a3a606b15919d24f2b8f63
qinxiaowen <453000697@qq.com>
2018-11-19 14:36:06 +0800

    part of mobie device banner invalid

--- src/Andoromeda.Kyubey.Portal/Controllers/LangController.cs
+++ src/Andoromeda.Kyubey.Portal/Controllers/LangController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Pomelo.AspNetCore.Localization;
 
 namespace Andoromeda.Kyubey.Portal.Controllers
 {
@@ -8,9 +9,14 @@ namespace Andoromeda.Kyubey.Portal.Controllers
         public IActionResult Index(string id)
         {
             Response.Cookies.Append("ASPNET_LANG", id);
-            return Redirect(string.IsNullOrWhiteSpace(Request.Headers["Referer"].ToString()) 
-                ? "/" 
+            return Redirect(string.IsNullOrWhiteSpace(Request.Headers["Referer"].ToString())
+                ? "/"
                 : Request.Headers["Referer"].ToString());
+        }
+        [Route("[controller]/Current")]
+        public IActionResult Current([FromServices] ICultureProvider _cultureProvider)
+        {
+            return Content(_cultureProvider.DetermineCulture());
         }
     }
 }
